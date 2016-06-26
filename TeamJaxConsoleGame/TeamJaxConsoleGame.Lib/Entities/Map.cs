6495b00bbec556f71c5d2923f17509dc96cf7db0
Ivan Maznov <ivan.maznov@gmail.com>
2016-06-26 03:38:47 +0300

    Added Map class and IMap interface.

---
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Entities/Map.cs
@@ -0,0 +1,27 @@
+﻿namespace TeamJaxConsoleGame.Lib.Entities
+{
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Map : IMap
+    {
+        public Map(int rows, int cols)
+        {
+            this.Rows = rows;
+            this.Cols = cols;
+        }
+
+        public int Cols { get; private set; }
+        public int Rows { get; private set; }
+
+        //Add Hero Method - Random?
+        public void AddHero(Hero hero)
+        {
+
+        }
+    }
+}
---
+++ TeamJaxConsoleGame/TeamJaxConsoleGame.Lib/Interfaces/IMap.cs
@@ -0,0 +1,19 @@
+﻿namespace TeamJaxConsoleGame.Lib.Interfaces
+{
+    using Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    interface IMap
+    {
+        int Rows { get; }
+        int Cols { get; }
+
+        void AddHero(Hero hero);
+
+
+    }
+}
